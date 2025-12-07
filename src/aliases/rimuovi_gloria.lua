39cd9dfa33597b5147b1d150d0f164aa2907255a
mauriliogenovese <125388969+mauriliogenovese@users.noreply.github.com>
2025-12-07 19:37:28 +0100

    echo per gloriarimuovi

--- src/aliases/rimuovi_gloria.lua
+++ src/aliases/rimuovi_gloria.lua
@@ -1,2 +1,7 @@
-SPAM.config.get("glory_timer")[matches[2]] = nil
-SPAM.config.save_characters()
+if SPAM.config.get("glory_timer")[matches[2]] ~= nil then
+    SPAM.config.get("glory_timer")[matches[2]] = nil
+    SPAM.config.save_characters()
+    cecho("<yellow>Rimuovo dalla lista gloria: "..matches[2])
+else
+    cecho("<red>"..matches[2].." non è presente nella lista gloria")
+end
